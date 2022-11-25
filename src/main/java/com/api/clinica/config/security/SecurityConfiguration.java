d385edc33360090c406b5d912d1e83741f9ee654
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-11-24 23:35:02 -0300

    security: liberando urls e metodos http para consumo do frontend

---
+++ src/main/java/com/api/clinica/config/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.api.clinica.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
